fc59490f7ee45807d5b5b651d08c91c345909459
chrisloop <chris@createaloop.org>
2021-02-10 12:03:18 -0600

    first commit

---
+++ Assets/_Toon/Custom Functions/MainLightData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class MainLightData : MonoBehaviour
+{
+	public Light mainLight;
+
+	void OnEnable()
+	{
+		mainLight = GetComponent<Light>();
+	}
+
+	
+	void Update ()
+	{
+		//Shader.SetGlobalVector("_LightDirection", -transform.forward);
+		//Shader.SetGlobalColor("_LightColor", mainLight.color);
+        Shader.SetGlobalFloat("_LightIntensity", mainLight.intensity); // can't figure out how to get light intensity in shader graph
+	}
+}
